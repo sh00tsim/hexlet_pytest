1dc1bcbab7e2448bef7095cabec29d0934f08029
Redduck <393201@mail.ru>
2021-07-30 21:59:59 +0500

    Add test emptiness

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -9,3 +9,12 @@ def test_stack():
     assert stack.pop() == 'two'
     assert stack.pop() == 'one'
 
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+
+    stack.pop()
+    assert not stack
+
